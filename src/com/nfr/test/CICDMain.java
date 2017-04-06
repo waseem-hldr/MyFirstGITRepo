0e48899a5e934f1bb0da9afc2f2a4e6ab0409f7a
Waseem Hawaldar <waseem.hawaldar@gmail.com>
2017-04-06 01:37:44 -0400

    Test NFR Project

---
+++ src/com/nfr/test/CICDMain.java
@@ -0,0 +1,10 @@
+package com.nfr.test;
+
+public class CICDMain {
+
+	
+	public static void main(String[] args) {
+		System.out.println("This is the main method to test CICD implementation");
+	}
+
+}
